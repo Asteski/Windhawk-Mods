78eaaec6352c0aaeac79e9933942aab1671c928a
asteski <asteski@outlook.com>
2026-02-15 14:24:11 +0100

    fixed toggle-taskbar-visibility bug

--- mods/asteski-toggle-taskbar-visibility.wh.cpp
+++ mods/asteski-toggle-taskbar-visibility.wh.cpp
@@ -2,7 +2,7 @@
 // @id           toggle-taskbar-visibility
 // @name         Toggle Taskbar Visibility (Ctrl+Alt+D)
 // @description  Toggle the Windows taskbar visibility using Ctrl+Alt+D. No prompts, no Explorer restart.
-// @version      0.1.0
+// @version      0.2.0
 // @author       Asteski
 // @include      explorer.exe
 // @compilerOptions -luser32
@@ -15,8 +15,55 @@ HWND g_hHotkeyWnd = NULL;
 void ToggleTaskbarVisibility() {
     HWND hTaskbar = FindWindowA("Shell_TrayWnd", NULL);
     if (hTaskbar) {
-        bool isVisible = IsWindowVisible(hTaskbar);
-        ShowWindow(hTaskbar, isVisible ? SW_HIDE : SW_SHOW);
+        BOOL isVisible = IsWindowVisible(hTaskbar);
+        if (isVisible) {
+            // Hide taskbar
+            ShowWindow(hTaskbar, SW_HIDE);
+            SetWindowPos(hTaskbar, NULL, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOZORDER | SWP_FRAMECHANGED);
+            // Set work area to full screen
+            RECT screenRect;
+            SystemParametersInfo(SPI_GETWORKAREA, 0, &screenRect, 0);
+            HMONITOR hMonitor = MonitorFromWindow(hTaskbar, MONITOR_DEFAULTTOPRIMARY);
+            MONITORINFO mi = { 0 };
+            mi.cbSize = sizeof(mi);
+            if (GetMonitorInfo(hMonitor, &mi)) {
+                SystemParametersInfo(SPI_SETWORKAREA, 0, &mi.rcMonitor, SPIF_SENDCHANGE);
+            }
+        } else {
+            // Show taskbar
+            ShowWindow(hTaskbar, SW_SHOW);
+            SetWindowPos(hTaskbar, NULL, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOZORDER | SWP_FRAMECHANGED);
+            // Restore work area to exclude taskbar
+            APPBARDATA abd = { 0 };
+            abd.cbSize = sizeof(abd);
+            abd.hWnd = hTaskbar;
+            if (SHAppBarMessage(ABM_GETTASKBARPOS, &abd)) {
+                RECT workArea = { 0 };
+                SystemParametersInfo(SPI_GETWORKAREA, 0, &workArea, 0);
+                // Only shrink if the taskbar is on the same monitor as the work area
+                HMONITOR hMonitor = MonitorFromWindow(hTaskbar, MONITOR_DEFAULTTOPRIMARY);
+                MONITORINFO mi = { 0 };
+                mi.cbSize = sizeof(mi);
+                if (GetMonitorInfo(hMonitor, &mi)) {
+                    RECT newWorkArea = mi.rcMonitor;
+                    // Adjust work area based on taskbar position
+                    if (abd.rc.top == mi.rcMonitor.top && abd.rc.bottom > abd.rc.top) {
+                        // Taskbar on top
+                        newWorkArea.top += (abd.rc.bottom - abd.rc.top);
+                    } else if (abd.rc.bottom == mi.rcMonitor.bottom && abd.rc.bottom > abd.rc.top) {
+                        // Taskbar on bottom
+                        newWorkArea.bottom -= (abd.rc.bottom - abd.rc.top);
+                    } else if (abd.rc.left == mi.rcMonitor.left && abd.rc.right > abd.rc.left) {
+                        // Taskbar on left
+                        newWorkArea.left += (abd.rc.right - abd.rc.left);
+                    } else if (abd.rc.right == mi.rcMonitor.right && abd.rc.right > abd.rc.left) {
+                        // Taskbar on right
+                        newWorkArea.right -= (abd.rc.right - abd.rc.left);
+                    }
+                    SystemParametersInfo(SPI_SETWORKAREA, 0, &newWorkArea, SPIF_SENDCHANGE);
+                }
+            }
+        }
     }
 }
 
